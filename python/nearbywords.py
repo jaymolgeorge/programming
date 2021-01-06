92dab2fc8b76a128b033a837767ba5ab3fc8df6c
jaymolgeorge <41983832+jaymolgeorge@users.noreply.github.com>
2021-01-06 01:15:44 -0800

    Create nearbywords.py

---
+++ python/nearbywords.py
@@ -0,0 +1,6 @@
+"""
+Facebook's How to Crush Your Coding Interview. https://www.facebook.com/video.php?v=10152735777427200. Problem: Write a function that given a string, returns all nearby words. You are given the following helper functions: Set<String> getNearbyChars(char c); isWord(String word);
+ref: https://gist.github.com/alexskyy/5acfe26739f80f750b61, https://gist.github.com/dpott197/b34bcc76deeb7a9cc451
+"""
+
+
